912189b9d60f5c4eca889c67de543e34e98ad43f
joohee <xorjsqnfl@gmail.com>
2021-09-21 03:41:00 +0900

    study - express 유용한  내부 미들웨어들

--- 11-express/app3-route.js
+++ 11-express/app3-route.js
@@ -3,7 +3,21 @@ import postRouter from './router/post.js'
 import userRouter from './router/user.js'
 
 const app = express()
-app.use(express.json())
+app.use(express.json()) // REST API -> Body parse
+app.use(express.urlencoded({ extended: false })) // HTML Form에서 Submmit을 하게 되면 발생하는 request를 Body 안으로 자동으로 parse 해줌.
+
+// http://expressjs.com/en/4x/api.html#express.static
+const options = {
+    dotfiles: 'ignore', // 숨겨진 파일은 무시
+    etag: false,
+    index: false,
+    maxAge: '1d', // 캐시 기간
+    redirect: false,
+    setHeaders: function (res, path, stat) { // 헤더에 필요한 데이터를 추가
+        res.set('x-timestamp', Date.now())
+    }
+}
+app.use(express.static('public', options)) // public 안에 있는 모든 resource에 대해 접근이 가능하도록 한다.
 
 app.use('/posts', postRouter)
 app.use('/users', userRouter)
