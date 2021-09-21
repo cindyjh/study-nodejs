d20449bd1621525fbfb6b95e5de4bb26605351c9
joohee <xorjsqnfl@gmail.com>
2021-09-21 23:56:08 +0900

    study - validator

---
+++ 14-validation/app.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import { body, param, validationResult } from 'express-validator'
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/users',
+    [
+        body('name')
+            .notEmpty()
+            .withMessage("이름을 입력해주세요")
+            .isLength({min: 2, max: 10})
+            .withMessage('이름은 두 글자 이상이어야 합니다.'),
+        body('age')
+            .notEmpty()
+            .isInt()
+            .withMessage("숫자를 입력해주세요"),
+        body('email')
+            .isEmail().withMessage('이메일 형식에 맞지 않습니다.'),
+        body('job.name').notEmpty()
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({message: errors.array()})
+        }
+        console.log(req.body)
+        if (req.body.email)
+        res.sendStatus(201)
+})
+
+app.get('/:email',
+[
+    param('email').isEmail().withMessage('이메일 필요함.')
+]
+, (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({message: errors.array()})
+    }
+    res.send('❤')
+})
+app.listen(8080)
